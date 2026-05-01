56bd37f4a388e1a0b0b3c9eb9c07eed1523bba2f
nainsworth <nja8161995@gmail.com>
2026-04-30 20:42:05 -0500

    Set up email sends

--- app/actions/email.ts
+++ app/actions/email.ts
@@ -1,17 +1,27 @@
 "use server";
 
 import type { FormData } from "@/components/forms/clientIntakeForm";
+import confirmationEmail from "@/emails/confirmationEmail";
+import IntakeEmail from "@/emails/intakeEmail";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendIntakeEmail = async (formData: FormData) => {
-  await resend.emails.send({
-    from: "Test <onboarding@resend.dev>",
-    to: ["nja8161995@gmail.com"],
-    subject: "New Client Intake Form",
-    html: `<p>${formData.name}</p>`,
-  });
+  await Promise.all([
+    resend.emails.send({
+      from: "Test <onboarding@resend.dev>",
+      to: ["nja8161995@gmail.com"],
+      subject: "New Client Intake Form",
+      react: IntakeEmail(formData),
+    }),
+    resend.emails.send({
+      from: "Test <onboarding@resend.dev>",
+      to: [formData.email],
+      subject: "We got your form!",
+      react: confirmationEmail({ name: formData.name }),
+    }),
+  ]);
 };
 
 export default sendIntakeEmail;
