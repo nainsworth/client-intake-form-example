0991a1072396b58f554c2644d9cee9a187dfac6c
nainsworth <nja8161995@gmail.com>
2026-05-18 20:09:50 -0500

    Added Analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import ClientIntakeForm from "@/components/forms/clientIntakeForm";
+import { Analytics } from "@vercel/analytics/next";
 
 const page = () => {
   return (
     <main className="min-h-screen flex justify-center m-8 md:m-24">
       <ClientIntakeForm />
+      <Analytics />
     </main>
   );
 };
